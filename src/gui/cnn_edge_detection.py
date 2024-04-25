76a9e92bf007cc46f651e539806292302dc8f405
SpeekeR <DominikZappe1999@seznam.cz>
2024-04-25 12:52:09 +0200

    Doc Added

--- src/gui/cnn_edge_detection.py
+++ src/gui/cnn_edge_detection.py
@@ -7,25 +7,38 @@ from torchvision import transforms
 from src.nn.net_loader import NetLoader
 
 
+# Canny edge detection model (analytical weights)
 canny_net = NetLoader.load_net("canny")
 
+# CNN edge detection model (big data, metacentrum training)
 cnn_big_net = NetLoader.load_net("cnn")
+# CNN edge detection model (big data, metacentrum training)
 cnn_big_net = NetLoader.load_weights(cnn_big_net, "../../models/edge_detection_model_only_bigger_data_meta.pth")
 
+# CNN edge detection model (my data, local training)
 cnn_my_net = NetLoader.load_net("cnn")
+# CNN edge detection model (my data, local training)
 cnn_my_net = NetLoader.load_weights(cnn_my_net, "../../models/edge_detection_model_my_data.pth")
 
+# HED "baseline" edge detection model (opencv caffe)
 hed_baseline_net = NetLoader.load_net("baseline")
 
+# HED edge detection model (big data, metacentrum training)
 hed_big_net = NetLoader.load_net("hed")
+# HED edge detection model (big data, metacentrum training)
 hed_big_net = NetLoader.load_weights(hed_big_net, "../../models/edge_detection_hed_model_meta.pth")
 
+# HED edge detection model (my data, local training)
 hed_my_net = NetLoader.load_net("hed")
+# HED edge detection model (my data, local training)
 hed_my_net = NetLoader.load_weights(hed_my_net, "../../models/edge_detection_hed_model_my_data.pth")
 
+# HED edge detection model (my data, local training, 1024x1024)
 hed_my_smoother_net = NetLoader.load_net("hed")
+# HED edge detection model (my data, local training, 1024x1024)
 hed_my_smoother_net = NetLoader.load_weights(hed_my_smoother_net, "../../models/edge_detection_hed_model_my_data_bigger_resize.pth")
 
+# Image transformations for CNN models
 cnn_img_transform = transforms.Compose([
     transforms.Resize((1024, 1024)),
     transforms.Lambda(lambda x: x.convert("RGB")),
@@ -33,6 +46,7 @@ cnn_img_transform = transforms.Compose([
     transforms.GaussianBlur(3, 3),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+# Image transformations for HED models
 hed_img_transform = transforms.Compose([
     transforms.Resize((1024, 1024)),
     transforms.Lambda(lambda x: x.convert("RGB")),
@@ -43,6 +57,12 @@ hed_img_transform = transforms.Compose([
 
 
 def canny_net_edge_detection(img, args):
+    """
+    Canny edge detection using analytical weights
+    :param img: Input image
+    :param args: Additional arguments (UNUSED, but required for compatibility)
+    :return: Edge detection result
+    """
     img = img.astype(float) / 255.0
     img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).float()
 
@@ -54,6 +74,12 @@ def canny_net_edge_detection(img, args):
 
 
 def preprocess_img(img, transform):
+    """
+    Preprocess image for CNN models
+    :param img: Input image
+    :param transform: Image transformation
+    :return: Preprocessed image, original width, original height
+    """
     orig_w, orig_h = img.shape[:2]
     img = Image.fromarray(img)
     img = transform(img)
@@ -62,6 +88,14 @@ def preprocess_img(img, transform):
 
 
 def postprocess_output(output, orig_w, orig_h, threshold=100):
+    """
+    Postprocess output of CNN models
+    :param output: Output from model (tensor)
+    :param orig_w: Original width
+    :param orig_h: Original height
+    :param threshold: Threshold for edge detection
+    :return: Edge detection result
+    """
     output = output.squeeze(0).detach().numpy()
     output = output[0, :, :]  # Convert to 2D
 
@@ -77,6 +111,12 @@ def postprocess_output(output, orig_w, orig_h, threshold=100):
 
 
 def cnn_big_edge_detection(img, args):
+    """
+    CNN edge detection using big data model
+    :param img: Input image
+    :param args: Additional arguments (threshold)
+    :return: Edge detection result
+    """
     threshold = args[0]
 
     img, orig_w, orig_h = preprocess_img(img, cnn_img_transform)
@@ -85,6 +125,12 @@ def cnn_big_edge_detection(img, args):
 
 
 def cnn_my_edge_detection(img, args):
+    """
+    CNN edge detection using my data model
+    :param img: Input image
+    :param args: Additional arguments (threshold)
+    :return: Edge detection result
+    """
     threshold = args[0]
 
     img, orig_w, orig_h = preprocess_img(img, cnn_img_transform)
@@ -93,6 +139,12 @@ def cnn_my_edge_detection(img, args):
 
 
 def hed_baseline_edge_detection(img, args):
+    """
+    HED edge detection using baseline model
+    :param img: Input image
+    :param args: Additional arguments (UNUSED, but required for compatibility)
+    :return: Edge detection result
+    """
     (orig_h, orig_w) = img.shape[:2]
     mean_pixel_values = np.average(img, axis=(0, 1))
     blob = cv2.dnn.blobFromImage(img, scalefactor=0.7, size=(orig_w, orig_h), mean=mean_pixel_values, swapRB=True, crop=False)
@@ -106,6 +158,12 @@ def hed_baseline_edge_detection(img, args):
 
 
 def hed_big_edge_detection(img, args):
+    """
+    HED edge detection using big data model
+    :param img: Input image
+    :param args: Additional arguments (threshold)
+    :return: Edge detection result
+    """
     threshold = args[0]
 
     img, orig_w, orig_h = preprocess_img(img, hed_img_transform)
@@ -114,6 +172,12 @@ def hed_big_edge_detection(img, args):
 
 
 def hed_my_edge_detection(img, args):
+    """
+    HED edge detection using my data model
+    :param img: Input image
+    :param args: Additional arguments (threshold)
+    :return: Edge detection result
+    """
     threshold = args[0]
 
     img, orig_w, orig_h = preprocess_img(img, hed_img_transform)
@@ -122,6 +186,12 @@ def hed_my_edge_detection(img, args):
 
 
 def hed_my_smoother_edge_detection(img, args):
+    """
+    HED edge detection using my data model with 1024x1024 input size
+    :param img: Input image
+    :param args: Additional arguments (threshold)
+    :return: Edge detection result
+    """
     threshold = args[0]
 
     img, orig_w, orig_h = preprocess_img(img, hed_img_transform)
